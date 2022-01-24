e3ecab586d5b29dbe559ff01506c6528e0fe9059
Praisecodes <cmancoder@gmail.com>
2022-01-24 01:51:26 +0100

    Disabling Maximize Button

--- MyForm.h
+++ MyForm.h
@@ -262,6 +262,7 @@ namespace PraiseCodesApp {
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(300, 400);
+			this->MaximizeBox = false;
 			this->Controls->Add(this->Btn1);
 			this->Controls->Add(this->Btn2);
 			this->Controls->Add(this->Btn3);
